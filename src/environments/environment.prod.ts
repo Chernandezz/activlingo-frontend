4f7fdc5385e6863acac10ab23828d9038f06815d
Cristian Hernandez <crissstianfhz@gmail.com>
2025-06-10 13:41:37 -0500

    env key added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: 'http://147.182.130.162:8000',
+  supabaseUrl: 'https://hhhmryqiikbnsmobwkza.supabase.co',
   supabaseAnonKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhoaG1yeXFpaWtibnNtb2J3a3phIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2MTg4NjUsImV4cCI6MjA2MzE5NDg2NX0.0VbMWL9UlZHvFo5Tx-TZDlCq5SLj8cxG-DOUj39lIMc',
+  useLocalStorage: true,
 };
