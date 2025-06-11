a0b33d5779e0cfd972b14a04d88d495e36154e39
Cristian Hernandez <crissstianfhz@gmail.com>
2025-06-10 21:19:23 -0500

    chore: auth moved first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,13 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from './features/auth/guards/auth.guard';
 
 export const routes: Routes = [
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./features/auth/pages/auth-page/auth-page.component').then(
+        (c) => c.AuthPageComponent
+      ),
+  },
   { path: '', redirectTo: 'chat', pathMatch: 'full' },
 
   {
@@ -20,25 +27,19 @@ export const routes: Routes = [
         './features/dictionary/pages/dictionary-page/dictionary-page.component'
       ).then((c) => c.DictionaryPageComponent),
   },
-  {
-    path: 'auth',
-    loadComponent: () =>
-      import('./features/auth/pages/auth-page/auth-page.component').then(
-        (c) => c.AuthPageComponent
-      ),
-  },
+
   {
     path: 'success',
     loadComponent: () =>
-      import('./features/onboarding/pages/success-page/success-page.component').then(
-        (m) => m.SuccessPageComponent
-      ),
+      import(
+        './features/onboarding/pages/success-page/success-page.component'
+      ).then((m) => m.SuccessPageComponent),
   },
   {
     path: 'cancel',
     loadComponent: () =>
-      import('./features/onboarding/pages/cancel-page/cancel-page.component').then(
-        (m) => m.CancelPageComponent
-      ),
+      import(
+        './features/onboarding/pages/cancel-page/cancel-page.component'
+      ).then((m) => m.CancelPageComponent),
   },
 ];
